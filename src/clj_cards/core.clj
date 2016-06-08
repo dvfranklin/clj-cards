3f8dc549715950b8966eb8182f6178be203a1e5d
Dan <dvfranklin@gmail.com>
2016-06-07 21:38:13 -0400

    Straight and straight flush working

--- src/clj_cards/core.clj
+++ src/clj_cards/core.clj
@@ -28,13 +28,22 @@
   ; if they're all the same, return true
   (= 1(count (set (map :suit hand)))))
 
+
 (defn straight? [hand]
   ; gets just the ranks from each hand
-  (let [hand (sort-hand hand)
-        ]
-    ))
+  (let [hand  (sort-hand hand)
+        ; pulls each card into an individual binding
+        c1    (nth hand 0)
+        c2    (nth hand 1)
+        c3    (nth hand 2)
+        c4    (nth hand 3)]
+    ; if each card is one higher than the previous, it's a straight
+    (and (= (inc c3) c4) (and (= (inc c2) c3) (= (inc c1) c2)))))
+
     ; if it's a straight but not a flush, return true
     ;(and straight (not flush))))
+
+
 
 (defn four-of-a-kind? [hand]
   ; collects a set of which ranks appear in the hand
@@ -44,8 +53,10 @@
 (defn straight-flush? [hand]
   ; check if it's a flush
   (let [flush (flush? hand)
+
         ; check if it's a straight
         straight (straight? hand)]
+
     ; if both, return true
     (and flush straight)))
 
@@ -53,12 +64,16 @@
 (defn three-of-a-kind? [hand]
   ; gets just the ranks from each card
   (let [hand (sort-hand hand)
+
         ; hash of cards & times they appear
         hand (frequencies hand)
+
         ; number of times first card appears
         firstrank (second (first hand))
+
         ; number of times second card appears
         secondrank (second (second hand))]
+
       ; if either one appears three times, return true
       (or (= 3 firstrank) (= 3 secondrank))))
 
@@ -66,12 +81,16 @@
 (defn two-pair? [hand]
   ; gets just the ranks from each card
   (let [hand (sort-hand hand)
+
         ; hash of cards & times they appear
         hand (frequencies hand)
+
         ; get the number of times the first card appears
         firstrank (second (first hand))
+
         ; get the number of times the second card appears
         secondrank (second (second hand))]
+
     ; if both cards appear twice, return true
     (and (= 2 firstrank) (= 2 secondrank))))
 
@@ -81,5 +100,5 @@
 (defn -main []
   (let [deck (create-deck)
         hands (create-hands deck)
-        two-pair (filter two-pair? hands)]
-    (println (count two-pair))))
+        straight (filter straight? hands)]
+    (println (count straight))))
